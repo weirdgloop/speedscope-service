66073e7c6eb30ecfc4ec377d2629a000cd5bdf17
SomeRandomDeveloper <git@srd.dev>
2026-03-10 22:02:55 +0100

    Fix .env path in scripts

--- scripts/run-in-container.sh
+++ scripts/run-in-container.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 IMAGE="ghcr.io/somemwdev/speedscope-service:main"
 
 docker run --rm \
-  --env-file .env \
+  --env-file "$SCRIPT_DIR/../.env" \
   -v "$(pwd)/db:/db:rw" \
   -p 3001:3000 \
   "$IMAGE" \
