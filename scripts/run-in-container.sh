82779493d00bd757ef9770afe964b24063c42712
SomeRandomDeveloper <git@srd.dev>
2026-03-16 22:32:32 +0100

    Update image name after move to weirdgloop org

--- scripts/run-in-container.sh
+++ scripts/run-in-container.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-IMAGE="ghcr.io/somemwdev/speedscope-service:main"
+IMAGE="ghcr.io/weirdgloop/speedscope-service:main"
 
 docker run --rm \
   --env-file "$SCRIPT_DIR/../.env" \
