7532f4c3ae5d8b59d89bd141f8a2dae9893a5d1c
Luka Simić <git@kocka.tech>
2026-04-08 06:48:15 +0200

    Add option to listen on a Unix socket.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
+import { existsSync, unlinkSync } from 'node:fs';
 import app from './app.js';
 import config from './config/config.js';
 
-app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
+if (config.unixSocket && existsSync(config.unixSocket)) {
+  unlinkSync(config.unixSocket);
+}
+app.listen(config.unixSocket ?? config.port, () => {
+  if (config.unixSocket) {
+    console.log(`Server running on Unix socket ${config.unixSocket}`);
+  } else {
+    console.log(`Server running on port ${config.port}`);
+  }
 });
